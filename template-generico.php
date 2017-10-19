a1310a3362c7f9576002a881d9648cbd4762bff0
nixlovemi <nixlovemi@gmail.com>
2017-10-19 09:42:35 -0200

    Template default e ajuste no footer.

---
+++ template-generico.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: Página Genérica - Decoratum */
+/* https://getbootstrap.com/examples/grid/ */
+get_header();
+?>
+
+<section class="product-cake sec-carrinho">
+    <div class="container">
+        <?php
+        the_content();
+        ?>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
